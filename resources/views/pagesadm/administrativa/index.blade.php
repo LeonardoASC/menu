96193ceccf90da9dbee6a2f6c9a2e4f66c899155
LeonardoASC <leonardoaschaves@gmail.com>
2023-08-10 18:41:01 -0300

    a

--- resources/views/pagesadm/administrativa/index.blade.php
+++ resources/views/pagesadm/administrativa/index.blade.php
@@ -275,7 +275,7 @@
                                                 Qnt. Pedidos Entregues</th>
                                         </tr>
                                     </thead>
-                                    <tbody class="w-full h-20 overflow-y-scroll">
+                                    <tbody class="w-full overflow-y-scroll">
                                         @if ($clientesAtivos->isEmpty())
                                             <p>Nenhum cliente no estabelecimento.</p>
                                         @else
@@ -307,7 +307,7 @@
 
                     <!-- Recent Activities -->
                     <div
-                        class="relative flex flex-col min-w-0 break-words bg-gray-50 dark:bg-gray-800 w-full shadow-lg rounded">
+                        class="relative flex flex-col min-w-0 break-words bg-gray-50 dark:bg-gray-800 w-full shadow-lg rounded ">
                         <div class="rounded-t mb-0 px-0 border-0">
                             <div class="flex flex-wrap items-center px-4 py-2">
                                 <div class="relative w-full max-w-full flex-grow flex-1">
@@ -321,7 +321,7 @@
                                 </div>
                             </div>
 
-                            <div class="block w-full">
+                            <div class="block w-full overflow-y-scroll">
                                 @foreach ($pedidosAgrupados as $data => $pedidos)
                                     <div
                                         class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
