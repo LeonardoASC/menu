9c2eb00282537782cc2d89fd86026db7000d32c3
LeonardoASC <leonardoaschaves@gmail.com>
2023-08-14 17:21:49 -0300

    trocando a rota home

--- resources/views/components/navbarheader.blade.php
+++ resources/views/components/navbarheader.blade.php
@@ -1,5 +1,5 @@
 <!-- Header -->
-<div class="fixed w-full flex items-center justify-between h-14 text-white z-10 block">
+<div class="fixed w-full flex items-center justify-between h-14 text-white z-10 ">
     <div
         class="flex items-center justify-start md:justify-center pl-3 w-14 md:w-64 h-14 bg-gray-800 dark:bg-gray-800 border-none">
         <img class="w-7 h-7 md:w-10 md:h-10 mr-2 rounded-md overflow-hidden"
