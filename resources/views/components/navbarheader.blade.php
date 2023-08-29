c418a82d0240e6b72f01b8d2833ebe6dd619f003
LeonardoASC <leonardoaschaves@gmail.com>
2023-08-29 18:19:20 -0300

    chatify

--- resources/views/components/navbarheader.blade.php
+++ resources/views/components/navbarheader.blade.php
@@ -102,7 +102,7 @@
                 </a>
             </li>
             <li>
-                <a href="{{ route('mensagem.index') }}"
+                <a href="{{ route('mensagem') }}"
                     class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-800 dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-gray-500 dark:hover:border-gray-800 pr-6">
                     <span class="inline-flex justify-center items-center ml-4">
                         <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,6 @@ Route::middleware(['auth'])->group(function () {
     Route::resource('mesa', 'App\Http\Controllers\MesaController');
     Route::resource('pedidoadm', 'App\Http\Controllers\PedidoADMController');
     Route::resource('administrativa', 'App\Http\Controllers\AdministrativaController');
-    Route::resource('mensagem', 'App\Http\Controllers\MensagemController');
     Route::resource('usuario', 'App\Http\Controllers\UsuarioController');
     Route::resource('role', 'App\Http\Controllers\RoleController');
     Route::resource('permission', 'App\Http\Controllers\PermissionController');
